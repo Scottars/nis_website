97305bb4f699ce54c61dd95301dca158509df860
Scottars <ScottarSEEE@hust.edu.cn>
2019-11-07 19:55:37 +0800

    增加了数据库的性能测试的部分，当前能够了解目前数据库能够每秒写多少次insert

--- dataservice/databaseoperation/redisreadwritespeedtest.py
+++ dataservice/databaseoperation/redisreadwritespeedtest.py
@@ -2,12 +2,18 @@
 
 import redis   # 导入redis模块，通过python操作redis 也可以直接在redis主机的服务端操作缓存数据库
 
+import time
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)   # host是redis主机，需要redis服务端和客户端都启动 redis默认端口是6379
 
-r.set('name', 'junxi')  # key是"foo" value是"bar" 将键值对存入redis缓存
-print(r['name'])
-print(r.get('name'))  # 取出键name对应的值
-print(type(r.get('name')))
+
+start_time = time.clock()
+for i in range(100000):
+    r.set('name', i)  # key是"foo" value是"bar" 将键值对存入redis缓存
+
+end_time = time.clock()
+
+
+print('实际花费的时间',end_time-start_time)
 
 
 
