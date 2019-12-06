4922da4009d9f4f493966940dc1db655b0a09a7f
Scottars <ScottarSEEE@hust.edu.cn>
2019-12-06 11:23:13 +0800

    修改了consumer类变成了异步的congsumer类的情况，其能够正常实现聊天的功能

--- chat/consumer.py
+++ chat/consumer.py
@@ -1,33 +1,36 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_group_name = 'ops_coffee'
-        # print('we are here1')
+
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-        # print('we are here2')
-        self.accept()
 
-    def disconnect(self, close_code):
+        await self.accept()
+
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -36,10 +39,60 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     # Receive message from room group
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = '运维咖啡吧：' + event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
+
+
+
+
+
+
+
+
+##下面是同步的写法，由于用到了channel——layer，所以一切都变成了异步，而因此，我们必须讲异步的通信消息，编程同步的内容
+# class ChatConsumer(WebsocketConsumer):
+#     def connect(self):
+#         self.room_group_name = 'ops_coffee'
+#         # print('we are here1')
+#         # Join room group
+#         async_to_sync(self.channel_layer.group_add)(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+#         # print('we are here2')
+#         self.accept()
+#
+#     def disconnect(self, close_code):
+#         # Leave room group
+#         async_to_sync(self.channel_layer.group_discard)(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+#
+#     # Receive message from WebSocket
+#     def receive(self, text_data):
+#         text_data_json = json.loads(text_data)
+#         message = text_data_json['message']
+#
+#         # Send message to room group
+#         async_to_sync(self.channel_layer.group_send)(
+#             self.room_group_name,
+#             {
+#                 'type': 'chat_message',
+#                 'message': message
+#             }
+#         )
+#
+#     # Receive message from room group
+#     def chat_message(self, event):
+#         message = '运维咖啡吧：' + event['message']
+#
+#         # Send message to WebSocket
+#         self.send(text_data=json.dumps({
+#             'message': message
+#         }))
--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from chat.consumer import ChatConsumer
 
+'''
+    这个routing 就相当于django框架中的urls 文件，所有关于ws/chat的内容，我们都交给chatsonsumer进行处理
+'''
+
 websocket_urlpatterns = [
     path('ws/chat/', ChatConsumer),
 ]
