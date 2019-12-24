4a396da7cf40d3c00260fca0fa2cca27fa93a13b
scottar <ScottarSEEE@hust.edu.cn>
2019-12-24 23:52:01 +0800

    实现了可以对注册的时候前台js对数据的验证的方式,然后提交form到后台....通过修改<input的 type 属性为buttom 和submit 的方式来实现切换,这样即使报错也不会提交到后台数据啦

--- myapp/views.py
+++ myapp/views.py
@@ -43,8 +43,53 @@ def  registersave(request):
         'form':form,
 
     }
-    return  render(request,'mywebsite/register_2.html',context)
+    return  render(request,'mywebsite/register.html',context)
 
+
+def regist(request):
+    if request.method == 'POST':
+        regi_form = NisUserInfo_form(request.POST)  #包含用户名和密码
+        if regi_form.is_valid():
+            #获取表单数据
+            username = regi_form.cleaned_data['username']  #cleaned_data类型是字典，里面是提交成功后的信息
+            password = regi_form.cleaned_data['password']
+            phone = regi_form.cleaned_data['password']
+            #添加到数据库
+            # registAdd = User.objects.get_or_create(username=username,password=password)
+            registAdd = NisUserInfo.objects.create(username=username, password=password,phone=phone)
+            # print registAdd
+            if registAdd == False:
+
+                print('注册失败')
+                return render(request,'share1.html', {'registAdd': registAdd, 'username': username})
+
+            else:
+                print('注册成功')
+                # return HttpResponse('ok')
+                return render(request,'share1.html', {'registAdd': registAdd})
+                # return render_to_response('share.html',{'registAdd':registAdd},context_instance = RequestContext(request))
+    else:
+        # 如果不是post提交数据，就不传参数创建对象，并将对象返回给前台，直接生成input标签，内容为空
+        regi_form = NisUserInfo_form(request.POST or None)
+    # return render_to_response('regist.html',{'uf':uf},context_instance = RequestContext(request))
+    return render(request,'mywebsite/register.html', {'form': regi_form})
+
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         print username,password
+#         re = auth.authenticate(username=username,password=password)  #用户认证
+#         if re is not None:  #如果数据库里有记录（即与数据库里的数据相匹配或者对应或者符合）
+#             auth.login(request,re)   #登陆成功
+#             return redirect('/',{'user':re})    #跳转--redirect指从一个旧的url转到一个新的url
+#         else:  #数据库里不存在与之对应的数据
+#             return render(request,'login.html',{'login_error':'用户名或密码错误'})  #注册失败
+#     return render(request,'login.html')
+#
+#
 
 
 
