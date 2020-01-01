7591482ef1cb427fb23a1fd6391e35124d264348
scottar <ScottarSEEE@hust.edu.cn>
2020-01-01 19:27:43 +0800

    echarts加入了可以通过滑轮进行数据的缩放的情况

--- static/mywebsite/js/datashow.js
+++ static/mywebsite/js/datashow.js
@@ -1266,8 +1266,33 @@ function drawhistory(result) {
     }
     // console.log(v_data_xy);
 
-
     option = {
+        dataZoom: [
+            {
+                type: 'slider',
+                xAxisIndex: 0,
+                start: 10,
+                end: 60
+            },
+            {
+                type: 'inside',
+                xAxisIndex: 0,
+                start: 10,
+                end: 60
+            },
+            {
+                type: 'slider',
+                yAxisIndex: 0,
+                start: 30,
+                end: 80
+            },
+            {
+                type: 'inside',
+                yAxisIndex: 0,
+                start: 30,
+                end: 80
+            }
+        ],
         xAxis: [{
             splitLine: {show: false},
             // show:false,
